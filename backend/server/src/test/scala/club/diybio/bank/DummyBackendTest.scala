192890bd5b4aca0973ca64028c84cb43f1d5b592
Michael Korbakov <rmihael@gmail.com>
2015-05-18 23:38:19 +0300

    Some changes in directories structure, dummy tests and Travis-CI integration

---
+++ backend/server/src/test/scala/club/diybio/bank/DummyBackendTest.scala
@@ -0,0 +1,11 @@
+package club.diybio.bank
+
+import utest._
+
+object DummyBackendTest extends TestSuite {
+  val tests = TestSuite {
+    "this is simple backend dummy test" - {
+      assert(true)
+    }
+  }
+}
---
+++ backend/shared/src/test/scala/club/diybio/bank/DummySharedTest.scala
@@ -0,0 +1,11 @@
+package club.diybio.bank
+
+import utest._
+
+object DummySharedTest extends TestSuite {
+  val tests = TestSuite {
+    "this is simple shared dummy test" - {
+      assert(true)
+    }
+  }
+}
